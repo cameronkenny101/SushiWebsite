14a8f188f792e780beb51ec4f319852b66dce5c0
Cameron Kenny <55995394+cameronkenny101@users.noreply.github.com>
2020-04-04 13:50:28 +0100

    Add files via upload

---
+++ Website/email.php
@@ -0,0 +1,17 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'cam.e.kenny@gmail.com';
+    $email_subject = "New Form Submit";
+    $email_body = "User Name : $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+
+    $to = "cameron.kenny1@ucdconnect.ie";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: contact.html");
+?>
